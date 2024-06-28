cbb83d8f4161f118e360ea4af55ebfe03e2b5791
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-06-28 21:02:27 +0530

    Deploy test

---
+++ app/(dashboard)/_components/Overview.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+// import CategoriesStats from "@/app/(dashboard)/_components/CategoriesStats";
+import StatsCards from "@/app/(dashboard)/_components/StatsCards";
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { MAX_DATE_RANGE_DAYS } from "@/lib/constants";
+import { UserSettings } from "@prisma/client";
+import { differenceInDays, startOfMonth } from "date-fns";
+import React, { useState } from "react";
+import { toast } from "sonner";
+import CategoriesStats from "./CategoriesStats";
+
+function Overview({ userSettings }: { userSettings: UserSettings }) {
+  const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
+    from: startOfMonth(new Date()),
+    to: new Date(),
+  });
+
+  return (
+    <>
+      <div className="container flex flex-wrap items-end justify-between gap-2 my-8">
+        <h2 className="text-4xl font-bold text-center">Overview</h2>
+        <div className="flex flex-col md:flex-row items-center gap-3">
+          <DateRangePicker
+            initialDateFrom={dateRange.from}
+            initialDateTo={dateRange.to}
+            showCompare={false}
+            onUpdate={(values) => {
+              const { from, to } = values.range;
+              // We update the date range only if both dates are set
+
+              if (!from || !to) return;
+              if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+                toast.error(
+                  `The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days!`
+                );
+                return;
+              }
+
+              setDateRange({ from, to });
+            }}
+          />
+        </div>
+      </div>
+      <div className="px-2 flex w-full flex-col gap-2 ">
+        
+        <StatsCards
+          userSettings={userSettings}
+          from={dateRange.from}
+          to={dateRange.to}
+        />
+
+        <CategoriesStats
+          userSettings={userSettings}
+          from={dateRange.from}
+          to={dateRange.to}
+        />
+      </div>
+    </>
+  );
+}
+
+export default Overview;
