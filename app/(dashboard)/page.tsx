0348b90473112f55c413235c4a1cf452c21f0877
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-06-28 22:18:39 +0530

    Success

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -28,7 +28,7 @@ async function page() {
     <div className="h-full bg-background">
       <div className="border-b bg-card">
         <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
-          <p className="text-3xl font-bold">Hello, {user.firstName}! 👋</p>
+          <p className="text-3xl font-bold">Hello, {user.firstName}</p>
 
           <div className="flex items-center gap-3">
             <CreateTransactionDialog
