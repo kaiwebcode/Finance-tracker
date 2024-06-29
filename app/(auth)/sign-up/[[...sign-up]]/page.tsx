7093f9721ea2f63a6fe67b0b9f8b5ca60b7a5225
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-06-30 02:08:29 +0530

    Deployment new

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -65,7 +65,7 @@ export default function Page() {
                 </h1>
 
                 <p className="mt-4 leading-relaxed text-gray-500">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  money, ipsum dolor sit amet consectetur adipisicing elit.
                   Eligendi nam dolorum aliquam, quibusdam aperiam voluptatum.
                 </p>
               </div>
