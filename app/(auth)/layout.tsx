57d502c3b2774c9791472eb6f48b122f275426dd
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-06-29 22:10:50 +0530

    auth update mt

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ function layout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
       {/* <Logo /> */}
-      <div className="mt-12">{children}</div>
+      <div>{children}</div>
     </div>
   );
 }
