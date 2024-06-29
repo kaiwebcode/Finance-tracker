b541964725af3aa88e0043d60c0b6f42f4d3f765
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-06-29 20:49:42 +0530

    update signin

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import React, { ReactNode } from "react";
 function layout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
-      <Logo />
+      {/* <Logo /> */}
       <div className="mt-12">{children}</div>
     </div>
   );
