3fd006cbf4a78bb620fde7d51633451876862561
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-06-30 02:05:29 +0530

    Deployment new

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -65,8 +65,9 @@ export default function Page() {
                 </h1>
 
                 <p className="mt-4 leading-relaxed text-gray-500">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Eligendi nam dolorum aliquam, quibusdam aperiam voluptatum.
+                  Ever feel like you're throwing money away? Take control fo
+                  your cashflow by logging what you've spent with this simple
+                  form.
                 </p>
               </div>
               <SignUp />
