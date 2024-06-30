49589f6943193ce1271260ec06039ae199b07566
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-06-30 21:10:42 +0530

    Deployment

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,14 +4,14 @@ import React from "react";
 
 function Logo() {
   return (
-    <a href="/" className="flex items-center gap-2">
+    <a href="/" className="flex items-center gap-2 py-3">
       {/* <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" /> */}
 
       {/* <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-4xl font-bold leading-tight tracking-tighter text-transparent">
         
       </p> */}
       <Image src="./logo.svg" alt="Logo" height={30} width={30} />
-      <p className="font-semibold text-white text-2xl ml-2">Financial-Tracker</p>
+      <p className="font-semibold text-white text-3xl ml-2">Financial-Tracker</p>
     </a>
   );
 }
