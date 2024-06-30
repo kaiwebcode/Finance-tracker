7aceb7b5b68900f8e030ddb9b9f2517a6cc1fb73
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-06-30 21:40:12 +0530

    Deployment

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -19,8 +19,8 @@ function Logo() {
 export function LogoMobile() {
   return (
     <a href="/" className="flex items-center gap-1">
-      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-2xl font-bold leading-tight tracking-tighter text-transparent">
-        Finance-Tracker
+      <p className=" text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Financial-Tracker
       </p>
     </a>
   );
