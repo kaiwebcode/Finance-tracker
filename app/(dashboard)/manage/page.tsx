b63e70327cd05546033708c1cc55e9d0329600ee
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-06-30 14:38:55 +0530

    Deployment

--- app/(dashboard)/manage/page.tsx
+++ app/(dashboard)/manage/page.tsx
@@ -131,7 +131,7 @@ function CategoryList({ type }: { type: TransactionType }) {
 
 function CategoryCard({ category }: { category: Category }) {
   return (
-    <div className="flex flex-col justify-between rounded-md m-6 border shadow-md shadow-black/[0.1] dark:shadow-white/[0.1]">
+    <div className="flex flex-col justify-between rounded-md m-2 border shadow-md shadow-black/[0.1] dark:shadow-white/[0.1]">
       <div className="flex flex-col items-center gap-2 p-4">
         <span className="text-3xl" role="img">
           {category.icon}
