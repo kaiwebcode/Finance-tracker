682e6badffa45d265172f1bd195ad233971387db
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-06-30 21:05:56 +0530

    Deployment new

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,4 +1,5 @@
 import { PiggyBank } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 function Logo() {
@@ -6,9 +7,11 @@ function Logo() {
     <a href="/" className="flex items-center gap-2">
       {/* <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" /> */}
 
-      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-4xl font-bold leading-tight tracking-tighter text-transparent">
-        Financial-Tracker
-      </p>
+      {/* <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-4xl font-bold leading-tight tracking-tighter text-transparent">
+        
+      </p> */}
+      <Image src="./logo.svg" alt="Logo" height={28} width={28} />
+      <p className="font-semibold text-white text-2xl ml-2.5">Finance</p>
     </a>
   );
 }
