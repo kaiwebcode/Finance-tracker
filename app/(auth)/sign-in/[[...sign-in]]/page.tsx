fd3d1fc3029493d707ea315bb702cfe533d21244
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-07-02 14:46:30 +0530

    Deployment again

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -60,7 +60,7 @@ export default function Page() {
               </a>
 
               <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                Welcome to Sign-up Page
+                Welcome to Sign-in Page
               </h1>
 
               <p className="my-4 leading-relaxed text-gray-500">
