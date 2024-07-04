1aeefc5dfedf48492398a0012fec3ddc72dcf4a1
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-07-04 21:46:19 +0530

    Deployment

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton, useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 function Header() {
+  const { user, isSignedIn } = useUser(); // Check if the hook is imported correctly
+
   return (
-    <div>Header is Here...</div>
-  )
+    <div className="p-5 flex justify-between items-center border shadow-lg">
+      <Image src={"/logo.svg"} alt="logo" width={160} height={100} />
+      {isSignedIn ? (
+        <UserButton />
+      ) : (
+        <Link href={"/sign-in"}>
+          <Button>Get Started</Button>
+        </Link>
+      )}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
