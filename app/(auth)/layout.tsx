0d520cd2d4b6efea84ff1ae838714cfef09cf87f
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-07-04 21:39:56 +0530

    Doployment

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,13 @@
 // import Logo from "@/components/Logo";
 import Logo from "@/components/Logo";
 import React, { ReactNode } from "react";
+import Header from "../components/Header";
 
 function layout() {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
       {/* <Logo /> */}
+      <Header />
       <div> Yoo Kai is Here..</div>
     </div>
   );
