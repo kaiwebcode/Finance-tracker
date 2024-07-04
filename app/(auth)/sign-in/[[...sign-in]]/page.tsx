45381bb09d951b723bc47c909a323556fe11b215
KaifQureshi78 <kaifqureshi.pr@gmail.com>
2024-07-04 22:24:03 +0530

    Deployment

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -64,8 +64,7 @@ export default function Page() {
               </h1>
 
               <p className="my-4 leading-relaxed text-gray-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi nam dolorum aliquam, quibusdam aperiam voluptatum.
+                Ever feel like you're throwing money away? 
               </p>
             </div>
 
